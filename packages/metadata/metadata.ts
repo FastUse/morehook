09457cf969f729514e94eb8ece896e5250458f08
cxh <2604856589@qq.com>
2022-10-29 23:25:58 +0800

    style

--- packages/metadata/metadata.ts
+++ packages/metadata/metadata.ts
@@ -5,20 +5,23 @@ import _metadata, {
   functions as _functions
 } from './index.json'
 
-// 类型标签列表
+// 类型标签列表排序
 const categoriesOrder = [
+  // 状态 - 对数据的状态更改捕捉
+  // 例如记录promise的等待以及成功状态，storage自动存取，一些双向的数据更改可以放这个分类
   'State',
-  'Elements',
-  'Browser',
-  'Sensors',
-  'Network',
-  'Animation',
-  'Component',
-  'Watch',
-  'Reactivity',
-  'Array',
-  'Time',
-  'Utilities'
+
+  'Elements', // 元素 - 涉及到原生 DOM 的hook
+  'Browser', // 浏览器 - 关于浏览器的API
+  'Sensors', // 传感器 - ps: 网络状态捕捉，鼠标动向捕捉，scroll捕捉
+  'Network', // 网络连接 - ps: http封装，websocket封装，fetch封装
+  'Animation', // 动画 - ps: settimeout封装，当前时间，过渡
+  'Component', // 组件 - 可以控制页面 ref元素
+  'Watch', // 监听 - 监听数组，状态这些
+  'Reactivity', // 反应 - (暂时不大清楚界限)
+  'Array', // 数组 - 关于数组的处理
+  'Time', // 时间 - 关于时间的处理
+  'Utilities' // 公共工具 - 转base64，转数字，eventBus这些
 ]
 
 export const metadata = _metadata as unknown as PackageIndexes
