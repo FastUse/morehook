f675cb215085d75d8bdfd595323d297bbaba8879
cxh <2604856589@qq.com>
2022-11-08 20:52:24 +0800

    style: useBoolean-demo更改

--- packages/core/useBoolean/demo.vue
+++ packages/core/useBoolean/demo.vue
@@ -14,15 +14,3 @@ import { useBoolean } from '@morehook/core'
 const [useBooleanState, { toggle: useBooleanToggle, setTrue, setFalse }] =
   useBoolean()
 </script>
-
-<style lang="scss" scoped>
-.demo {
-  button {
-    color: #fff;
-    margin-right: 20px;
-    padding: 0px 6px;
-    background-color: #999;
-    border-radius: 10px;
-  }
-}
-</style>
