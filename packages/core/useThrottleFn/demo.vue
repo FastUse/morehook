468d9fe02b6c1144ad2e1bfd5de3ff33b95a8a7b
cxh <2604856589@qq.com>
2022-11-08 19:46:11 +0800

    feat: useThrottleFn

--- packages/core/useThrottleFn/demo.vue
+++ packages/core/useThrottleFn/demo.vue
@@ -3,6 +3,7 @@
     <p>num: {{ num }}</p>
 
     <c-button @click="run">执行函数 +1</c-button>
+    <c-button @click="runTrue">执行函数 +1(最后一次执行有效)</c-button>
   </div>
 </template>
 
@@ -11,7 +12,18 @@ import { ref } from 'vue'
 import { useThrottleFn } from '@morehook/core'
 
 const num = ref(0)
-const { run } = useThrottleFn(() => {
-  num.value++
-}, 1000)
+const { run: runTrue } = useThrottleFn(
+  () => {
+    num.value++
+  },
+  1000,
+  true
+)
+const { run } = useThrottleFn(
+  () => {
+    num.value++
+  },
+  1000,
+  false
+)
 </script>
--- packages/core/useThrottleFn/index.ts
+++ packages/core/useThrottleFn/index.ts
@@ -6,13 +6,52 @@ type Fn = (...params: any[]) => any
  * @param delay 节流设定时间
  * @returns 包装后的函数
  */
-const throttle = function (this: any, fn: Fn, delay: number) {
+// const throttle = function (this: any, fn: Fn, delay: number) {
+//   let oldNow = Date.now()
+//   return (...args: []) => {
+//     const currNow = Date.now()
+//     if (currNow - oldNow < delay) return
+//     oldNow = currNow
+//     fn.call(this, ...args)
+//   }
+// }
+
+/**
+ * 节流
+ * @param fn 待执行函数
+ * @param delay 节流设定时间
+ * @param runLastFn 是否在节流过期时执行最后一次被拦住的方法
+ * @returns 包装后的函数
+ */
+const throttle = function (
+  this: any,
+  fn: Fn,
+  delay: number,
+  runLastFn = false
+) {
   let oldNow = Date.now()
+  let lastFn = () => {
+    // 最后拦住的方法
+  }
+  let timmer: NodeJS.Timeout | null = null
+
   return (...args: []) => {
     const currNow = Date.now()
-    if (currNow - oldNow < delay) return
-    oldNow = currNow
-    fn.call(this, ...args)
+
+    if (currNow - oldNow < delay) {
+      if (runLastFn) {
+        lastFn = fn
+        timmer && clearInterval(timmer)
+        timmer = setInterval(() => {
+          lastFn.call(this, ...args)
+          timmer && clearInterval(timmer)
+        }, delay)
+      }
+    } else {
+      if (runLastFn && timmer) clearInterval(timmer)
+      oldNow = currNow
+      fn.call(this, ...args)
+    }
   }
 }
 
@@ -22,9 +61,14 @@ const defaultDelay = 1000
  * 处理节流函数
  * @param fn 待执行函数
  * @param delay 节流设定时间 (默认 1000)
+ * @param runLastFn 是否在节流过期时执行最后一次被拦住的方法
  * @returns 包装后的函数
  */
-export function useThrottleFn(fn: Fn, delay?: number) {
-  const run = throttle(fn, typeof delay === 'number' ? delay : defaultDelay)
+export function useThrottleFn(fn: Fn, delay?: number, runLastFn = false) {
+  const run = throttle(
+    fn,
+    typeof delay === 'number' ? delay : defaultDelay,
+    runLastFn
+  )
   return { run }
 }
