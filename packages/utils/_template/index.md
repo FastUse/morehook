54a1fb2f137e96bf85de0da9aa0f5fbd9ce3ccaf
cxh <2604856589@qq.com>
2022-11-26 14:39:27 +0800

    chore: 添加@utils 示例模板

---
+++ packages/utils/_template/index.md
@@ -0,0 +1,14 @@
+---
+category: utils
+---
+
+# _template
+
+我是功能描述:
+
+## Usage
+``` js
+import { getRandomStr } from '@morehook/utils'
+
+const key = getRandomStr()
+```
---
+++ packages/utils/_template/index.ts
@@ -0,0 +1,12 @@
+/**
+ * 获取唯一的随机字符串
+ * @param randomLength 生成的随机字符串长度（最高为12）
+ * @return 11位或者12位的字符串，当randomLength小于11时不保证唯一性
+ */
+export function _template(randomLength?: number): string
+
+export function _template(randomLength = 5) {
+  return Number(Math.random().toString().slice(3, 8) + Date.now())
+    .toString(36)
+    .slice(0, randomLength)
+}
